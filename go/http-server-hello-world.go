fcccd74927c86fe146f13f04619d10801c9120aa
Dmitry Nikitenko <dima.nikitenko@gmail.com>
2020-04-25 13:25:55 +0600

    simple golang server

---
+++ go/http-server-hello-world.go
@@ -0,0 +1,18 @@
+// go build http-server.go
+// ./http-server
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
